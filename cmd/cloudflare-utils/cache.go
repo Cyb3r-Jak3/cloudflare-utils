20ddb5ff8088613d006d21da693892e256fe5068
Cyb3r-Jak3 <git@Cyberjake.xyz>
2025-10-04 23:38:56 -0400

    Simplify cache purging to single API call

--- cmd/cloudflare-utils/cache.go
+++ cmd/cloudflare-utils/cache.go
@@ -49,7 +49,8 @@ func CacheCleaner(ctx context.Context, c *cli.Command) error {
 	urls := c.StringSlice("url")
 	tags := c.StringSlice("tag")
 	prefixes := c.StringSlice("prefix")
-	if !everything && len(urls) == 0 && len(tags) == 0 && len(prefixes) == 0 {
+	hosts := c.StringSlice("host")
+	if !everything && len(urls) == 0 && len(tags) == 0 && len(prefixes) == 0 && len(hosts) == 0 {
 		return fmt.Errorf("must specify at least one purge method: --everything, --url, --tag, or --prefix")
 	}
 	if (everything && len(urls) > 0) || (everything && len(tags) > 0) || (everything && len(prefixes) > 0) {
@@ -73,41 +74,27 @@ func CacheCleaner(ctx context.Context, c *cli.Command) error {
 		logger.Info("Successfully purged everything")
 		return nil
 	}
+	purgeRequest := cloudflare.PurgeCacheRequest{}
 	if len(urls) > 0 {
 		logger.Infof("Purging %d URLs from cache", len(urls))
-		_, cleanErr := APIClient.PurgeCache(ctx, zoneRC.Identifier, cloudflare.PurgeCacheRequest{
-			Files: urls,
-		})
-		if cleanErr != nil {
-			logger.WithError(cleanErr).Error("Error purging URLs")
-			return cleanErr
-		}
-		logger.Info("Successfully purged URLs")
-		return nil
+		purgeRequest.Files = urls
 	}
 	if len(tags) > 0 {
 		logger.Infof("Purging %d tags from cache", len(tags))
-		_, cleanErr := APIClient.PurgeCache(ctx, zoneRC.Identifier, cloudflare.PurgeCacheRequest{
-			Tags: tags,
-		})
-		if cleanErr != nil {
-			logger.WithError(cleanErr).Error("Error purging tags")
-			return cleanErr
-		}
-		logger.Info("Successfully purged tags")
-		return nil
+		purgeRequest.Tags = tags
 	}
 	if len(prefixes) > 0 {
 		logger.Infof("Purging %d prefixes from cache", len(prefixes))
-		_, cleanErr := APIClient.PurgeCache(ctx, zoneRC.Identifier, cloudflare.PurgeCacheRequest{
-			Prefixes: prefixes,
-		})
-		if cleanErr != nil {
-			logger.WithError(cleanErr).Error("Error purging prefixes")
-			return cleanErr
-		}
-		logger.Info("Successfully purged prefixes")
-		return nil
+		purgeRequest.Prefixes = prefixes
+	}
+	if len(hosts) > 0 {
+		logger.Infof("Purging %d hosts from cache", len(hosts))
+		purgeRequest.Hosts = hosts
+	}
+	_, err = APIClient.PurgeCache(ctx, zoneRC.Identifier, purgeRequest)
+	if err != nil {
+		logger.WithError(err).Error("Error purging cache")
+		return err
 	}
 	return nil
 }
